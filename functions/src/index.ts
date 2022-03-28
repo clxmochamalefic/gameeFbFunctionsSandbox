7e4f0a59708e4032126fda57b768aabfd5b65a9e
cocoalix <cocoalix@gmail.com>
2022-03-29 01:50:54 +0900

    create voice channel on discord

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,29 @@
 import * as functions from "firebase-functions";
+import {Client, Intents} from "discord.js";
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+const myServerId = "<DISCORD_SERVER_ID>";
+const token = "<DISCORD_BOT_TOKEN>";
+
+const client = new Client({intents: [Intents.FLAGS.GUILDS]});
+client.on("ready", () => {
+  functions.logger.log(client?.user?.tag);
+  const myServer = client.guilds.cache.get(myServerId);
+  myServer?.channels.create("TestVoicChannel", {
+    type: "GUILD_VOICE",
+  });
+  functions.logger.log("Create Voice Channel");
+});
+
+exports.helloWorld = functions.https.onRequest((request, response) => {
+  response.send("Hello from Firebase!");
+});
+
+exports.createVoiceChannel = functions.https.onRequest((request, response) => {
+  response.send(client?.user?.tag);
+  client.login(token).then((value) => {
+    functions.logger.log("logged in");
+  }).catch((error) => {
+    functions.logger.log("login failure" + error);
+  });
+  response.send("AAAA");
+});
